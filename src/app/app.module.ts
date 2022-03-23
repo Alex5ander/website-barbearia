7f8e4a1b7d249f5385a500c34d92c53de4c2ff8d
Alexsander Gutierrez Gonçalves <alexsandergutierreez@gmail.com>
2022-03-23 12:41:30 +0000

    Adicionado biblioteca de mapas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+import { environment } from 'src/environments/environment';
+import { MapComponent } from './map/map.component';
+environment
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    MapComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken:'pk.eyJ1IjoiYWxleDVhbmRlciIsImEiOiJjbDEydmR1aXMzOGVnM2xvcHdobjd6dzlhIn0.hIqWvFm8sdNF_HbSWwpzeQ',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
