1f15b964d2a0cec61a8a681b930fab41d746ac11
Alexsander Gutierrez Gonçalves <alexsandergutierreez@gmail.com>
2022-04-05 23:45:38 +0000

    Edição

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'website-barbearia';
+  url = 'http://www.lucasreno.kinghost.net/barbearia';
+
+  constructor (public httpClient : HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get(this.url).subscribe(resposta => {
+      console.log(resposta);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -7,7 +8,6 @@ import { FooterComponent } from './footer/footer.component';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { environment } from 'src/environments/environment';
 import { MapComponent } from './map/map.component';
-import { CustomersComponent } from './customers/customers.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ServicesComponent } from './services/services.component';
 
@@ -17,7 +17,6 @@ import { ServicesComponent } from './services/services.component';
     HeaderComponent,
     FooterComponent,
     MapComponent,
-    CustomersComponent,
     GalleryComponent,
     ServicesComponent,
   ],
@@ -25,7 +24,8 @@ import { ServicesComponent } from './services/services.component';
     BrowserModule,
     NgxMapboxGLModule.withConfig({
       accessToken:'pk.eyJ1IjoiYWxleDVhbmRlciIsImEiOiJjbDE4cHBsNm4wZmp4M2JteGk4a2RvbDBkIn0.9bH1y2cWAKnu0NFUJo2k_g',
-    })
+    }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
